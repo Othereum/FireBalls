20c41cb81741b4df59cebb3ac6c6086e3d619e92
Othereum <seokjin.dev@gmail.com>
2020-07-30 14:53:31 +0900

    style: explicit namespace

--- Source/FireBalls/Private/FireBalls.cpp
+++ Source/FireBalls/Private/FireBalls.cpp
@@ -1,11 +1,9 @@
 #include "Engine.hpp"
 #include "FireBallsGame.hpp"
 
-using namespace fb;
-
 DEFINE_GAME_MODULE(Fire Balls);
 
-GAME_API void GameMain(Engine& engine)
+GAME_API void GameMain(oeng::Engine& engine)
 {
-	engine.GetWorld().SpawnActor<AFireBallsGame>();
+	engine.GetWorld().SpawnActor<fb::AFireBallsGame>();
 }
