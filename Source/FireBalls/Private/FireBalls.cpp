e82650024c4c28b5f42f90b42dbe3618ee07a91a
Othereum <seokjin.dev@gmail.com>
2020-07-30 14:39:54 +0900

    feat: FireBalls

--- Source/FireBalls/Private/FireBalls.cpp
+++ Source/FireBalls/Private/FireBalls.cpp
@@ -1,8 +1,11 @@
-#include <Engine.hpp>
+#include "Engine.hpp"
+#include "FireBallsGame.hpp"
+
+using namespace fb;
 
 DEFINE_GAME_MODULE(Fire Balls);
 
-GAME_API void GameMain(oeng::Engine&)
+GAME_API void GameMain(Engine& engine)
 {
-	
+	engine.GetWorld().SpawnActor<AFireBallsGame>();
 }
---
+++ Source/FireBalls/Public/FireBalls.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "EngineFwd.hpp"
+
+namespace fb
+{
+	using namespace oeng;
+	class ACharacter;
+	class AFireBallsGame;
+	class ABall;
+}
