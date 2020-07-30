901483cc6a6dd5a472829aafafb5e9062375ac75
Othereum <seokjin.dev@gmail.com>
2020-07-30 14:53:37 +0900

    feat: Character movement

--- Source/FireBalls/Private/Character.cpp
+++ Source/FireBalls/Private/Character.cpp
@@ -1,5 +1,7 @@
 #include "Character.hpp"
+#include "World.hpp"
 #include "Camera/CameraComponent.hpp"
+#include "Components/InputComponent.hpp"
 #include "Components/MovementComponent.hpp"
 
 namespace fb
@@ -10,5 +12,33 @@ namespace fb
 		camera_{AddComponent<CameraComponent>()}
 	{
 		SetRootComponent(&camera_);
+
+		auto& input = AddComponent<InputComponent>();
+		input.BindAxis(u8"MoveForward"sv, [this](Float f) { MoveForward(f); });
+		input.BindAxis(u8"MoveRight"sv, [this](Float f) { MoveRight(f); });
+		input.BindAxis(u8"Turn"sv, [this](Float f) { Turn(f); });
+		input.BindAction(u8"Fire"sv, true, [this]() { Fire(); });
+	}
+
+	void ACharacter::MoveForward(Float f) const noexcept
+	{
+		if (IsNearlyZero(f)) return;
+		movement_.AddMovInput(*GetForward() * f);
+	}
+
+	void ACharacter::MoveRight(Float f) const noexcept
+	{
+		if (IsNearlyZero(f)) return;
+		movement_.AddMovInput(*GetRight() * f);
+	}
+
+	void ACharacter::Turn(Float f) const noexcept
+	{
+		const auto delta = GetWorld().GetDeltaSeconds();
+		movement_.AddRotInput({UVec3::up, 360_deg * f * delta});
+	}
+
+	void ACharacter::Fire() const
+	{
 	}
 }
--- Source/FireBalls/Public/Character.hpp
+++ Source/FireBalls/Public/Character.hpp
@@ -10,6 +10,11 @@ namespace fb
 		explicit ACharacter(World& world);
 
 	private:
+		void MoveForward(Float f) const noexcept;
+		void MoveRight(Float f) const noexcept;
+		void Turn(Float f) const noexcept;
+		void Fire() const;
+		
 		MovementComponent& movement_;
 		CameraComponent& camera_;
 	};
